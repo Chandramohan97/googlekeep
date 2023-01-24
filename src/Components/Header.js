a138cc7b3c5911ee70746696e749cc905b36a9ce
Chandramohan97 <chandramsriv@gmail.com>
2023-01-24 20:54:26 +0530

    feat : Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="nav-bar">
+      <ul>
+        <li>
+          {" "}
+          <a
+            href="https://www.flaticon.com/free-icons/menu"
+            title="menu icons"
+          ></a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
