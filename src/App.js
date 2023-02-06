c570eaa420e9c0b44e98d1223c28f5822e471d3f
Chandramohan97 <chandramsriv@gmail.com>
2023-02-06 16:40:08 +0530

    feat : adding Inpurt bar to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header";
+import InputBar from "./Components/InputBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <InputBar />
     </div>
   );
 }
